d083c2865ab2a079ed3ebd12555f37a96e979136
Simon <DefaltSimon@users.noreply.github.com>
2016-09-25 16:53:14 +0200

    Update for dependencies

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 setup(
     name='SSKJpy',
     author="DefaltSimon",
-    version='0.1.1',
+    version='0.2',
     license='MIT',
     packages=["sskjpy"],
-    description="A Slovenian dictionary parser written in python",
+    description="A Slovenian dictionary scrapper written in python",
     long_description=open('README.txt').read(),
     install_requires=[
-        "requests >= 2.9.1",
         "beautifulsoup4 >= 4.4.1",
     ],
 )
